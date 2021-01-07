614698ca2a26dec840019b0f9cfbc60c3b8c870e
Dheeraj Bhatia <dheerajb@buffalo.edu>
2021-01-07 23:30:25 +0000

    Implemented correct public Api

--- js/lab.js
+++ js/lab.js
@@ -9,6 +9,19 @@ function readTextFile(file, callback) {
     }
     rawFile.send(null);
 }
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+
+        anHttpRequest.open( "GET", aUrl, true );    
+        anHttpRequest.setRequestHeader("Content-Type", "application/json");        
+        anHttpRequest.send( null );
+    }
+}
 
 function team(){
    var myObj = [
@@ -27,6 +40,15 @@ function team(){
         var obj = myObj[i];
     
         console.log(obj.id);
+
+        var client = new HttpClient();
+    client.get('https://pub.orcid.org/v3.0/'+obj.id, function(response) {
+
+    // do something with response
+        console.log(response);
+
+
+}); 
     }
 
     }
